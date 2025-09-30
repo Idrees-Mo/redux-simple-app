5d58287be4235c571cfb277b2f3f34c3b5ba0a81
Muhammad Idrees <idris@y7mail.com>
2025-09-30 22:49:10 +1000

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <div
-      style={{ maxWidth: "400px", margin: "2rem auto", textAlign: "center" }}
+      style={{
+        minWidth: "100vw",
+        margin: "2rem auto",
+        textAlign: "center",
+      }}
     >
       <h2>Redux Todo App</h2>
       <TodoForm />
