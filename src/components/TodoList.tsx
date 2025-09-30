badb7daf8f04f6ea3159716c98407487dca3bd1f
Muhammad Idrees <idris@y7mail.com>
2025-10-01 09:17:47 +1000

    wip: fetch on req only

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useAppSelector, useAppDispatch } from "../redux/hooks";
+// import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import TodoItem from "./TodoItem";
 import type { Todo } from "../redux/types";
 import { selectError, selectLoading, selectTodos } from "../redux/selectors";
@@ -13,22 +13,33 @@ const TodoList: React.FC = () => {
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   dispatch(fetchTodos());
+  // }, [dispatch]);
+
+  const handleFetchTodos = () => {
     dispatch(fetchTodos());
-  }, [dispatch]);
+  };
 
   if (loading) return <p>Loading todos...</p>;
   if (error) return <p style={{ color: "red" }}>Error: {error}</p>;
 
   return (
-    <ul
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      {todos.length === 0 && <p>No todos yet!</p>}
-      {todos.map((todo: Todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
+    <>
+      <button onClick={handleFetchTodos}>Fetch Todos</button>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {todos.length === 0 && <p>No todos yet!</p>}
+        {todos.map((todo: Todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </>
   );
 };
 
